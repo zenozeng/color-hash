86c9ec55789f28e7ec3682b8da60f4981b5762a6
Zeno Zeng <zenoofzeng@gmail.com>
2015-02-07 21:00:42 +0800

    build UMD package

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+## Usage
+
+### Browser
+
+A UMD version of ColorHash was located in dist/.
+
+### Node.JS
+
+```bash
+npm install color-hash --save
+```
+
+```javascript
+var ColorHash = require('color-hash');
+```
+
 ## FAQ
 
 ### Why not LAB?
@@ -5,3 +21,12 @@
 虽然 LAB 的色彩分布对人眼要更加线性一些，但是色域要更大，转换过程要更加复杂。
 考虑的一个关键点在于，HSL 色彩空间可以很方便地控制参数，
 比如亮度和饱和度，这样生成出来的颜色都是一个比较均匀和谐的分布。
+
+## Dev
+
+### Build browser js
+
+```bash
+sudo npm install -g browserify
+npm run build
+```
---
+++ dist/color-hash.js
@@ -0,0 +1,85 @@
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self),o.ColorHash=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var BKDRHash = function(str) {
+    var seed = 131;
+    var hash = 0;
+    for(var i = 0; i < str.length; i++) {
+        hash = hash * seed + str.charCodeAt(i);
+    }
+    return hash;
+};
+
+/**
+ * Color Hash Class
+ *
+ * @class
+ */
+var ColorHash = function(options) {
+    if(options.colors) {
+        this.pool = options.colors.concat(); // copy colors
+    }
+
+    var isArray = function(obj) {
+        return Object.prototype.toString.call(obj) === '[object Array]';
+    };
+
+    this.L = options.lightness || 0.5;
+    this.S = options.saturation || 0.5;
+    this.L = isArray(this.L) ? this.L.concat() : [this.L];
+    this.S = isArray(this.S) ? this.S.concat() : [this.S];
+
+    this.hash = options.hash || BKDRHash;
+};
+
+ColorHash.prototype.fromPool = function(str) {
+    var hash = this.hash(str);
+    if(this.pool) {
+        return this.pool[hash % this.pool.length];
+    } else {
+        throw new Error('options.colors undefined');
+    }
+};
+
+/**
+ * Returns the hash in [h, s, l]
+ * @param {String} str string to hash
+ * @returns {Array} [h, s, l]
+ */
+ColorHash.prototype.hsl = function(str) {
+    var H, S, L;
+    var hash = this.hash(str);
+
+    H = hash % 360;
+    hash = parseInt(hash / 360);
+    S = hash % this.S.length;
+    hash = parseInt(hash / this.S.length);
+    L = hash % this.L.length;
+
+    return [H, S, L];
+};
+
+/**
+ * Returns the hash in [r, g, b]
+ * @param {String} str string to hash
+ * @returns {Array} [r, g, b]
+ */
+ColorHash.prototype.rgb = function(str) {
+    var hsl = this.hsl(str);
+    // TODO: convert hsl to rgb
+    var rgb;
+    return rgb;
+};
+
+/**
+ * Returns the hash in hex
+ * @param {String} str string to hash
+ * @returns {String} hex with #
+ */
+ColorHash.prototype.hex = function(str) {
+    var rgb = this.rgb(str);
+    return '#' + rgb[0].toString(16) + rgb[1].toString(16) + rgb[2].toString(16);
+};
+
+module.exports = ColorHash;
+
+},{}]},{},[1])(1)
+});
