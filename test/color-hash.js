3d3621ed47628ec11c0189c18b80b5f8c7971fc8
Zeno Zeng <zenoofzeng@gmail.com>
2015-02-12 18:50:25 +0800

    rewrite tests, fixes #15

--- test/color-hash.js
+++ test/color-hash.js
@@ -6,21 +6,21 @@ describe('ColorHash', function() {
     describe('#HSL', function() {
         it('should return hash in HSL', function() {
             var colorHash = new ColorHash();
-            assert.deepEqual(colorHash.hsl(''), [0, 0.5, 0.5]);
+            assert.deepEqual(colorHash.hsl(''), [0, 0.35, 0.35]);
         });
     });
 
     describe('#RGB', function() {
         it('should return the hash in rgb', function() {
             var colorHash = new ColorHash();
-            assert.deepEqual(colorHash.rgb(''), [191, 64, 64]);
+            assert.deepEqual(colorHash.rgb(''), [120, 58, 58]);
         });
     });
 
     describe('#HEX', function() {
         it('should return the hash in hex', function() {
             var colorHash = new ColorHash();
-            assert.equal(colorHash.hex(''), '#bf4040');
+            assert.equal(colorHash.hex(''), '#783a3a');
         });
     });
 
