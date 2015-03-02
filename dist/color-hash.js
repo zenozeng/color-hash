7f0bb7a6fa98449ce0b86b442622cee5c32681c9
Zeno Zeng <zenoofzeng@gmail.com>
2015-03-02 19:14:04 +0800

    use 137 as seed2 to reduce hash collisions, fixes #16

--- dist/color-hash.js
+++ dist/color-hash.js
@@ -7,11 +7,13 @@
  */
 var BKDRHash = function(str) {
     var seed = 131;
+    var seed2 = 137;
     var hash = 0;
-    var MAX_SAFE_INTEGER = parseInt(9007199254740991 / seed / seed); // Number.MAX_SAFE_INTEGER equals  9007199254740991
+    // Note: Number.MAX_SAFE_INTEGER equals 9007199254740991
+    var MAX_SAFE_INTEGER = parseInt(9007199254740991 / seed2);
     for(var i = 0; i < str.length; i++) {
         if(hash > MAX_SAFE_INTEGER) {
-            hash = parseInt(hash / seed);
+            hash = parseInt(hash / seed2);
         }
         hash = hash * seed + str.charCodeAt(i);
     }
