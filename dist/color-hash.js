34f5e0ef412cbb2b33561f6bc4f318e5bee78022
Zeno Zeng <zenoofzeng@gmail.com>
2015-03-05 15:55:04 +0800

    make hash more sensitive for short string, fixes #18

--- dist/color-hash.js
+++ dist/color-hash.js
@@ -1,6 +1,6 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self),o.ColorHash=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
- * BKDR Hash
+ * BKDR Hash (modified version)
  *
  * @param {String} str string to hash
  * @returns {Number}
@@ -9,6 +9,7 @@ var BKDRHash = function(str) {
     var seed = 131;
     var seed2 = 137;
     var hash = 0;
+    str += 'x';
     // Note: Number.MAX_SAFE_INTEGER equals 9007199254740991
     var MAX_SAFE_INTEGER = parseInt(9007199254740991 / seed2);
     for(var i = 0; i < str.length; i++) {
