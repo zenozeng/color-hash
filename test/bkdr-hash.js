e08a3fb1619eb845269468e31f355629685af2cc
Zeno Zeng <zenoofzeng@gmail.com>
2015-05-18 23:57:04 +0800

    update unit tests, remove hard code tests

--- test/bkdr-hash.js
+++ test/bkdr-hash.js
@@ -2,9 +2,8 @@ var BKDRHash = require('../lib/bkdr-hash.js');
 var assert = require('assert');
 
 describe('BKDRHash', function() {
-    it('should return the BKDRHash of the given string', function() {
-        assert.equal(BKDRHash('abc'), 1677554);
-        assert.equal(BKDRHash('hij'), 1798605);
+    it('should return different value for different string', function() {
+        assert.notEqual(BKDRHash('abc'), BKDRHash('hij'));
     });
 
     it('should work even if the string is very long', function() {
--- test/color-hash.js
+++ test/color-hash.js
@@ -3,33 +3,11 @@ var assert = require('assert');
 
 describe('ColorHash', function() {
 
-    describe('#HSL', function() {
-        it('should return hash color in HSL', function() {
-            var colorHash = new ColorHash();
-            assert.deepEqual(colorHash.hsl(''), [0, 0.35, 0.35]);
-            assert.deepEqual(colorHash.hsl('hkl'),
-                             [colorHash.hash('hkl') % 359, 0.5, 0.35]);
-        });
-    });
-
-    describe('#RGB', function() {
-        it('should return the hash color in rgb', function() {
-            var colorHash = new ColorHash();
-            assert.deepEqual(colorHash.rgb(''), [120, 58, 58]);
-        });
-    });
-
-    describe('#HEX', function() {
-        it('should return the hash color in hex', function() {
-            var colorHash = new ColorHash();
-            assert.equal(colorHash.hex(''), '#783a3a');
-        });
-    });
-
     describe('#Lightness & Saturation', function() {
         it('should return the hash color based on the given lightness and saturation', function() {
             var colorHash = new ColorHash({lightness: 0.5, saturation: 0.5});
-            assert.deepEqual(colorHash.hsl(''), [0, 0.5, 0.5]);
+            var hsl = colorHash.hsl('');
+            assert.deepEqual([hsl[1], hsl[2]], [0.5, 0.5]);
         });
 
         it('should return the hash color based on the given lightness array and saturation array', function() {
@@ -37,7 +15,8 @@ describe('ColorHash', function() {
                 lightness: [0.9, 1],
                 saturation: [0.9, 1]
             });
-            assert.deepEqual(colorHash.hsl(''), [0, 0.9, 0.9]);
+            var hsl = colorHash.hsl('');
+            assert.deepEqual([hsl[1], hsl[2]], [0.9, 0.9]);
         });
     });
 
