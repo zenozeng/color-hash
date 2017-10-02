c297e69b0d1e43292eddb7fef6caea0f441c318e
Hugo Josefson <hugo@josefson.org>
2017-10-02 16:58:07 +0200

    Re-generate browser build.

--- dist/color-hash.js
+++ dist/color-hash.js
@@ -1,4 +1,4 @@
-!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self),o.ColorHash=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.ColorHash = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
  * BKDR Hash (modified version)
  *
@@ -94,6 +94,13 @@ var ColorHash = function(options) {
 
     this.L = LS[0];
     this.S = LS[1];
+    
+    this.minH = options.minH;
+    this.maxH = options.maxH;
+    if (typeof this.minH === 'undefined' && typeof this.maxH !== 'undefined')
+        this.minH = 0
+    if (typeof this.minH !== 'undefined' && typeof this.maxH === 'undefined')
+        this.maxH = 360
 
     this.hash = options.hash || BKDRHash;
 };
@@ -110,6 +117,10 @@ ColorHash.prototype.hsl = function(str) {
     var hash = this.hash(str);
 
     H = hash % 359; // note that 359 is a prime
+    if (typeof this.minH !== 'undefined' && typeof this.maxH !== 'undefined'){
+      H /= 1000
+      H = H * (this.maxH - this.minH) + this.minH;
+    }
     hash = parseInt(hash / 360);
     S = this.S[hash % this.S.length];
     hash = parseInt(hash / this.S.length);
