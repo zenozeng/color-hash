7daf4c3c6a6241f0b90bc94b8252d9eff87dcf03
Simone Picciani <zanza00@gmail.com>
2019-12-23 23:23:57 +0100

    test(algo): add retrocompatibility tests
    add some handpicked string using the old algoritms

--- src/__test__/color-hash-long-strings.test.ts
+++
@@ -1,9 +0,0 @@
-import ColorHash from "../color-hash";
-
-describe("long strings", () => {
-  const colorHash = new ColorHash();
-  it("Hello world", () => {
-    expect(colorHash.hex("Hello World")).toEqual("#AE87C5");
-    // expect(colorHash.hex("hello world")).toEqual("#AE87C5");
-  });
-});
